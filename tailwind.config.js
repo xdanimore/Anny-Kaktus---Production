1fc803eccb17e47f28f7f5af6549a152f4991af7
pulgueta <retardix456@gmail.com>
2022-04-26 02:16:29 -0500

    Tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
           mwhite: "#ffffff",
         },
       },
+
+      textColor: {
+        flora: {
+          base: "#8ad167",
+          hover: "#dfde9f",
+          black: "#1c1c1a",
+          mwhite: "#ffffff",
+        },
+      },
     },
   },
   plugins: [],
