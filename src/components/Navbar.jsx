1d45603d7b3569febffe3125d8ce42e633de7711
pulgueta <retardix456@gmail.com>
2022-04-26 04:33:11 -0500

    FINALLY NAVBAR

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,57 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {RiCloseLine, RiMenu3Fill} from "react-icons/ri";
+import { RiCloseLine, RiMenu3Fill } from "react-icons/ri";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+
   return (
-    <nav className="w-full h-20 flex justify-around items-center px-4 bg-flora-base text-flora-mwhite">
-      <Link className="text-2xl font-bold" to={'/'}>Anny Kaktus</Link>
+    <nav className="w-full h-20 flex justify-around items-center bg-flora-base text-flora-mwhite">
+      <Link className="text-3xl font-bold" to={"/"}>
+        Anny Kaktus
+      </Link>
 
-      <div>
-        <ul>
-          <li><Link to={'/'}>Inicio</Link></li>
-          <li><Link to={'/'}>Productos</Link></li>
-          <li><Link to={'/'}>Contacto</Link></li>
-          <li><Link to={'/'}>Carrito</Link></li>
-        </ul>
+      <ul className="hidden md:flex font-medium">
+        <li className="nav-link">
+          <Link to={"/"}>Inicio</Link>
+        </li>
+        <li className="nav-link">
+          <Link to={"/productos"}>Productos</Link>
+        </li>
+        <li className="nav-link">
+          <Link to={"/contacto"}>Contacto</Link>
+        </li>
+        <li className="nav-link">
+          <Link to={"/carrito"}>Carrito</Link>
+        </li>
+      </ul>
+
+      <div className="text-xl md:hidden z-10" onClick={handleClick}>
+        {isOpen ? <RiCloseLine /> : <RiMenu3Fill />}
       </div>
 
-      <div>
-        <RiMenu3Fill />
-      </div>
+      <ul
+        className={
+          !isOpen
+            ? "hidden"
+            : "absolute top-0 left-0 w-[75vw] h-screen bg-flora-base flex flex-col justify-center items-center"
+        }
+      >
+        <li className="mobile">
+          <Link to={"/"}>Inicio</Link>
+        </li>
+        <li className="mobile">
+          <Link to={"/productos"}>Productos</Link>
+        </li>
+        <li className="mobile">
+          <Link to={"/contacto"}>Contacto</Link>
+        </li>
+        <li className="mobile">
+          <Link to={"/carrito"}>Carrito</Link>
+        </li>
+      </ul>
     </nav>
-  )
+  );
 };
 export default Navbar;
