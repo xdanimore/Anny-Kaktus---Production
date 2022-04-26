679f73986821c1b461c53c0e963ec051178df5dc
pulgueta <retardix456@gmail.com>
2022-04-26 02:15:53 -0500

    Tailwind colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,7 @@ import {MenuAlt3Icon} from '@heroicons/react/outline'
 const Navbar = () => {
   return (
     <nav className='bg-green-500 h-20 px-8 flex justify-between items-center lg:px-24'>
-        <a className='text-3xl text-white font-bold cursor-pointer lg:text-4xl'>Anny Kaktus</a>
-
-        <ul className='text-white font-medium hidden md:flex'>
-            <li><a className='px-8 cursor-pointer'>Inicio</a></li>
-            <li><a className='px-8 cursor-pointer'>Productos</a></li>
-            <li><a className='px-8 cursor-pointer'>Contacto</a></li>
-            <li><a className='px-8 cursor-pointer'>Carrito</a></li>
-        </ul>
-
-        <div className='lg:hidden'>
-            <MenuAlt3Icon />
-        </div>
+        <Link to={'/'} className="text-2xl font-extrabold text-white">Anny Kaktus</Link>
     </nav>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        flora: {
+          base: "#8ad167",
+          hover: "#dfde9f",
+          black: "#1c1c1a",
+          mwhite: "#ffffff",
+        },
+      },
+    },
   },
   plugins: [],
 };
