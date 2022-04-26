365fbaf3581337c085febd24c69b078b3be13c5f
pulgueta <retardix456@gmail.com>
2022-04-26 04:14:21 -0500

    OMG

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import {MenuAlt3Icon} from '@heroicons/react/outline'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {RiCloseLine, RiMenu3Fill} from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <nav className='bg-green-500 h-20 px-8 flex justify-between items-center lg:px-24'>
-        <Link to={'/'} className="text-2xl font-extrabold text-white">Anny Kaktus</Link>
+    <nav className="w-full h-20 flex justify-around items-center px-4 bg-flora-base text-flora-mwhite">
+      <Link className="text-2xl font-bold" to={'/'}>Anny Kaktus</Link>
+
+      <div>
+        <ul>
+          <li><Link to={'/'}>Inicio</Link></li>
+          <li><Link to={'/'}>Productos</Link></li>
+          <li><Link to={'/'}>Contacto</Link></li>
+          <li><Link to={'/'}>Carrito</Link></li>
+        </ul>
+      </div>
+
+      <div>
+        <RiMenu3Fill />
+      </div>
     </nav>
   )
-}
-
-export default Navbar
+};
+export default Navbar;
