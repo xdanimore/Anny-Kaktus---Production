eba315d403707670c8df9b695a5957ff0fae2a2f
pulgueta <retardix456@gmail.com>
2022-04-30 01:13:51 -0500

    loading

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 import Monitor from "../assets/Render-Monitor-Angle1.jpg";
 
 const Card = (props) => {
   const id = useParams();
+  const nav = useNavigate();
+
   return (
     <div className="h-96 w-80 rounded-xl shadow-md">
       <div>
@@ -17,18 +17,18 @@ const Card = (props) => {
         />
       </div>
       <div className="py-4 px-6 h-[200px] overflow-hidden flex flex-col justify-between">
-        <h1 className="font-bold text-xl">{props.title || <Skeleton />}</h1>
+        <h1 className="font-bold text-xl">{props.title}</h1>
         <p className="text-flora-black text-sm">
-          {props.description || <Skeleton />}
+          {props.description}
         </p>
         <div className="flex justify-between items-center mt-4">
-          <h1 className="font-bold text-xl">{props.price || <Skeleton />}</h1>
-          <Link
+          <h1 className="font-bold text-xl">{props.price}</h1>
+          <button
             className="bg-flora-second py-1 px-4 rounded-md text-flora-white font-semibold transition-colors duration-300 hover:bg-flora-secondhover"
-            to={'/productos/:id'}
+            onClick={() => nav(`/productos/${id}`)}
           >
             Comprar
-          </Link>
+          </button>
         </div>
       </div>
     </div>
