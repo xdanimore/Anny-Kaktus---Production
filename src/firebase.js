f78569938f9de2b9a356c8618f9d837b3514c3e6
pulgueta <retardix456@gmail.com>
2022-05-01 17:57:01 -0500

    ?

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,16 @@ import { collection, getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "hola",
+  apiKey: import.meta.env.VITE_FIREBASE_KEY || "",
   authDomain: "anny-kaktus.firebaseapp.com",
   projectId: "anny-kaktus",
   storageBucket: "anny-kaktus.appspot.com",
   messagingSenderId: "471743892676",
-  appId: "hola",
+  appId: import.meta.env.VITE_FIREBASE_ID || "",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const productos = collection(db, "productos");
-export const auth = getAuth();
+// export const auth = getAuth();
