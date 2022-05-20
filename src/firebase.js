b2db1dd63c11adb4d8a22bb24c54beaf61ec4f2c
pulgueta <retardix456@gmail.com>
2022-05-20 15:48:06 -0500

    captcha

--- src/firebase.js
+++ src/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('AIzaSyDVUY0qmQNXvgENN8fTswJIRspqeIaikDc'),
+  provider: new ReCaptchaV3Provider(''),
   isTokenAutoRefreshEnabled: true,
 })
 const analytics = getAnalytics(app);
