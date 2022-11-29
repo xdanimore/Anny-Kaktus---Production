0348fe3605d86dcea5f00f65063ccb7940b95afd
pulgueta <retardix456@gmail.com>
2022-11-29 01:01:47 -0500

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Anny Kactus",
+        short_name: "AnnyKactus",
+        description: "Tienda virtual de plantas decorativas",
+        theme_color: "#4baf56",
+        icons: [
+          {
+            src: "/src/assets/favicon.ico",
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: { globPatterns: ["**/*.{js,css,html,ico,png,svg}"] },
+    }),
+  ],
+});
